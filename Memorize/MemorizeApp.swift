4b7d9b58fa9a9c0cb6a346386b8229157babbc1a
b-b-m <brunomotabertol@gmail.com>
2021-09-12 12:25:00 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bruno Mota on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
